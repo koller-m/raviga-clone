f45571be97e5b49401a171e6a1bb3d6644100a2c
Mike Koller <mkoller@eml.cc>
2022-11-07 11:40:56 -0700

    Create Hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import './Hero.css'
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div className="content">
+                <p>We invest in moonshot entrepreneurs to decrease the cost of living for all people.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
